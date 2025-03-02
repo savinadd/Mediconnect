a47540b4b36b132fc1bd0e9e69f9654132e2e629
Savina Dimitrova <96501769+savinadd@users.noreply.github.com>
2025-03-02 19:39:22 +0200

    fix gitignore in backend

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Medical Health App API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
